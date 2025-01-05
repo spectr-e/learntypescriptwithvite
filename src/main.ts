e9c8df4cf3c261754640e9b12d82f32d5e0b820b
spectr-e <devs.josia@gmail.com>
2025-01-05 23:33:56 +0300

    going through lesson one to sixteen

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+type Person = {
+	name: string
+	age: number
+	isProgrammer?: boolean
+	friends: string[]
+	address?: {
+		street: string
+	}
+}
+const person1: Person = {
+	name: "John",
+	age: 67,
+	friends: [],
+}
+const person2: Person = {
+	name: "Jane",
+	friends: [],
+	age: 73,
+	isProgrammer: true,
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type Options = {debugMode?: boolean; logLevel?: number}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function foo(
+	name: string = "Jos",
+	{debugMode = false, logLevel}: Options = {}
+) {
+	console.log(name, logLevel, debugMode)
+}
+
+function foo2(a: number, b: number, cb: (sum: number) => void) {
+	cb(a + b)
+}
+const result = foo2(3, 4, (sum) => {
+	console.log(sum)
+})
